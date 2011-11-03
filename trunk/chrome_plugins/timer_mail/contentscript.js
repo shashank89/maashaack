166431dc07ae5117d637a4fe5ed517ed82aa5157
gg.shily@gmail.com <gg.shily@gmail.com@be703901-0acc-1bb5-3cc7-5eb1d2cb26a8>
2011-11-03 09:59:24 +0000

    + add projects

---
+++ trunk/chrome_plugins/timer_mail/contentscript.js
@@ -0,0 +1,29 @@
+﻿
+console.log("start");
+
+// if has subject
+console.log(document.getElementById(":qi"));
+console.log(document.getElementById(":qc"));
+console.log(document.getElementById(":pz"));
+
+
+var path = [1, 1, 0, 0, 2,
+			0, 1, 0, 0, 0,
+			0, 0, 0, 0, 0,
+			0, 0, 0, 0];
+
+var element = document.documentElement;
+for(var i = 0; i < path.length; ++i)
+{
+	var index = path[i];
+	console.log(i + " index:" + index);
+	
+	var children = element.childNodes;
+	console.log("children" + children);
+	console.log(children.length);
+	
+	element = children[index];
+}
+
+console.log(element);
+//console.log(document.documentElement.children[1])
