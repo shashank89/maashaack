1e4d839dbb6c334b17f1ba65016f5f287a628659
gg.shily@gmail.com <gg.shily@gmail.com@be703901-0acc-1bb5-3cc7-5eb1d2cb26a8>
2011-11-03 10:52:32 +0000

    - only add comments for resolving a bug

--- chrome_plugins/jiraComment/contentscript.js
+++ chrome_plugins/jiraComment/contentscript.js
@@ -3,7 +3,7 @@ console.log("start");
 
 // if has subject
 
-var versionNumber;
+var versionNumber = "0.0.1";
 
 chrome.extension.onRequest.addListener(
   function(request, sender, sendResponse) {
@@ -19,14 +19,26 @@ document.addEventListener("DOMNodeInserted",
 	{
 		var commentTextarea = document.getElementsByName("comment");
 		
-		if(commentTextarea.length == 2 && !commentTextarea[1].value)
+		if(commentTextarea.length == 2 
+			&& !commentTextarea[1].value
+			&& getParentNode(commentTextarea[1], 9).id == "workflow-transition-5-dialog")
 		{
+			
 			commentTextarea[1].value = "fixed in version " + versionNumber;
 			
 			console.log("setted");
 		}
 	});
-		
+
+var getParentNode = function(element, deep){
+
+	for(var i = 0; i < deep; ++i)
+	{
+		element = element.parentNode;
+	}
+	
+	return element;
+}
 		
 /*var commentDiv = document.getElementById("workflow-transition-5-dialog");
 console.log(commentDiv);
