a6425c6c67dfd9e4e321d9fdfc87404ae34dad2a
gg.shily@gmail.com <gg.shily@gmail.com@be703901-0acc-1bb5-3cc7-5eb1d2cb26a8>
2012-08-23 07:55:32 +0000

    - update timer mail

--- chrome_plugins/timer_mail/contentscript.js
+++ chrome_plugins/timer_mail/contentscript.js
@@ -2,12 +2,61 @@
 console.log("start");
 
 // if has subject
-console.log(document.getElementById(":qi"));
-console.log(document.getElementById(":qc"));
-console.log(document.getElementById(":pz"));
+//console.log(document.getElementById(":qi"));
+//console.log(document.getElementById(":qc"));
+//console.log(document.getElementById(":pz"));
 
+function xpath(query, context){
+	return document.evaluate(context?(query.indexOf('.')==0?query:'.' + query):query, context || document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+};
 
-var path = [1, 1, 0, 0, 2,
+function nodeInsertedHandler(evt)
+	{
+		var element = document.getElementById(":qc");
+		if(element)
+		{
+			console.log("intert:");
+			console.log(evt.target);
+			
+			element.value = "change log by"; // NOTICE: the value is changed by other script later.
+			console.log(element.value);
+			
+			document.removeEventListener("DOMNodeInserted", nodeInsertedHandler);
+		}
+	};
+
+document.addEventListener("DOMNodeInserted", nodeInsertedHandler);
+
+var t = setTimeout(function ()
+	{
+		var firstResult = xpath('//*[@id=":q0"]', document).snapshotItem(0);//first result
+		var element = document.getElementById(":q0");
+		if(firstResult)
+		{
+			console.log("get button");
+			console.log(element);
+			clearTimeout(t);
+			if(document.createEvent)
+			{
+				var evtObj = document.createEvent('MouseEvents');
+				evtObj.initEvent('click', true, false);
+				element.dispatchEvent(evtObj);
+				console.log(evtObj);
+				console.log("send mail 1");
+				
+				
+				var element1 = document.getElementById(":pz");
+				element1.dispatchEvent(evtObj);
+			}
+			if(document.createEventObject)
+			{
+				element.fireEvent('onclick');
+				console.log("send mail 2");
+			}
+		}
+	}, 5000);
+
+/*var path = [1, 1, 0, 0, 2,
 			0, 1, 0, 0, 0,
 			0, 0, 0, 0, 0,
 			0, 0, 0, 0];
@@ -25,5 +74,5 @@ for(var i = 0; i < path.length; ++i)
 	element = children[index];
 }
 
-console.log(element);
+console.log(element);*/
 //console.log(document.documentElement.children[1])
