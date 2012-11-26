5f615371185f8264c053664bbc1bae3b28b7db68
gg.shily@gmail.com <gg.shily@gmail.com@be703901-0acc-1bb5-3cc7-5eb1d2cb26a8>
2012-11-26 13:39:03 +0000

    add script of removing weibo recommand

---
+++ trunk/chrome_plugins/weiboRecommend.user.js
@@ -0,0 +1,26 @@
+﻿// ==UserScript==
+// @name              remove weibo recommed
+// @namespace         weibo
+// @description       remove weibo recommed
+// @include           weibo.com/*
+// @version           0.01
+/* @reason
+@end*/
+
+(function() {
+	function $(w){
+		return document.getElementById(w.substring(1));
+	};
+	
+	function xpath(query, context){
+		return document.evaluate(context?(query.indexOf('.')==0?query:'.' + query):query, context || document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+	};
+	
+	var firstResult = xpath('//*[@id="workflow-transition-5-dialog"]', document).snapshotItem(0);//first result
+	
+	if(firstResult != null)
+	{
+		document.removeChild(firstResult);
+	}
+	
+})();
