1d4f7d1493f299d4f50c7fc844555faf0e390487
gg.shily@gmail.com <gg.shily@gmail.com@be703901-0acc-1bb5-3cc7-5eb1d2cb26a8>
2012-11-26 14:26:16 +0000

    update weiboRecommend.user.js

--- trunk/chrome_plugins/weiboRecommend.user.js
+++ trunk/chrome_plugins/weiboRecommend.user.js
@@ -3,7 +3,7 @@
 // @namespace         weibo
 // @description       remove weibo recommed
 // @include           weibo.com/*
-// @version           0.01
+// @version           0.02
 
 // ==/UserScript==
 
@@ -16,12 +16,22 @@
 		return document.evaluate(context?(query.indexOf('.')==0?query:'.' + query):query, context || document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
 	};
 	
-	var firstResult = xpath('//*[@node-type="feed_list_recommend"]', document).snapshotItem(0);//first result
-	
-	if(firstResult != null)
-	{
-		console.log(firstResult);
-		firstResult.parentNode.removeChild(firstResult);
+	function remove(){
+		var firstResult = xpath('//*[@node-type="feed_list_recommend"]', document).snapshotItem(0);//first result
+		
+		if(firstResult != null)
+		{
+			console.log(firstResult);
+			firstResult.parentNode.removeChild(firstResult);
+		}
 	}
+	
+	remove();
+	
+	document.addEventListener("DOMNodeInserted",
+	function(evt)
+	{
+		remove();
+	});
 	
 })();
